58cca340d8636baa3656df09d2adf7c0788f54ab
İsmail Sumer Cetin <sumercetin3@gmail.com>
2020-11-11 01:03:25 +0300

    Prettier code edit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import { Button, FormControl, InputLabel, Input, Grid, Select, MenuItem, makeStyles } from '@material-ui/core';
-import Task from './Task'
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  Input,
+  Grid,
+  Select,
+  MenuItem,
+  makeStyles,
+} from "@material-ui/core";
+import Task from "./Task";
 
 //firebase and its config file
-import db from './firebase';
-import firebase from 'firebase';
+import db from "./firebase";
+import firebase from "firebase";
 
 //css
-import './App.css';
+import "./App.css";
 
-import SnackbarComponent from './SnackbarComponent'
+import SnackbarComponent from "./SnackbarComponent";
 
 const useStyles = makeStyles(() => ({
   formControl: {
     minWidth: 120,
-    marginRight: "12px"
-  }
+    marginRight: "12px",
+  },
 }));
 
 export default function App() {
-  
   const classes = useStyles();
   const [tasks, setTasks] = useState([]);
   const [users, setUsers] = useState([]);
-  const [input, setInput] = useState('');
-  const [inputUser, setInputUser] = useState('');
+  const [input, setInput] = useState("");
+  const [inputUser, setInputUser] = useState("");
   const [handleSnackbar, setHandleSnackbar] = useState(false);
 
   useEffect(() => {
     //useEffect remembers what's inside
     //When there is a snapshot (when there's a change in db), fire this again
     try {
-      db.collection('users').onSnapshot(snapshot => {
-        setUsers(snapshot.docs.map(doc => ({id: doc.id, name: doc.data().name})))
+      db.collection("users").onSnapshot((snapshot) => {
+        setUsers(
+          snapshot.docs.map((doc) => ({ id: doc.id, name: doc.data().name }))
+        );
       });
     } catch (error) {
       alert(error);
     }
-    
   }, []);
 
   useEffect(() => {
@@ -46,97 +55,114 @@ export default function App() {
     //.orderBy('timestamp', 'desc') index error
 
     try {
-      db.collection('tasks').orderBy('timestamp', 'desc').where("ownerId", "==", inputUser).onSnapshot(snapshot => {
-        setTasks(snapshot.docs.map(doc => ({id: doc.id, 
-          task: doc.data().task, 
-          timestamp: doc.data().timestamp, 
-          isCompleted: doc.data().isCompleted,
-          taskColor: doc.data().taskColor
-        })))
-      });
+      db.collection("tasks")
+        .orderBy("timestamp", "desc")
+        .where("ownerId", "==", inputUser)
+        .onSnapshot((snapshot) => {
+          setTasks(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              task: doc.data().task,
+              timestamp: doc.data().timestamp,
+              isCompleted: doc.data().isCompleted,
+              taskColor: doc.data().taskColor,
+            }))
+          );
+        });
     } catch (error) {
       alert(error);
     }
-
   }, [inputUser]);
-  
 
   const addTask = (event) => {
     event.preventDefault();
- 
+
     try {
-      db.collection('tasks').add({
+      db.collection("tasks").add({
         task: input,
         timestamp: firebase.firestore.FieldValue.serverTimestamp(), //timestamp of the server
         ownerId: inputUser,
         isCompleted: false,
-        taskColor: '#ffffff'
-      })
-      setInput('');
+        taskColor: "#ffffff",
+      });
+      setInput("");
       setHandleSnackbar(true);
-
     } catch (error) {
       alert(error);
     }
-    
-  }
+  };
 
   const handleSnackbarClose = (event, reason) => {
-    if (reason === 'clickaway') {
-        return;
+    if (reason === "clickaway") {
+      return;
     }
 
     setHandleSnackbar(false);
   };
 
   function getSelectedUsername() {
-      const name = document.querySelector("#demo-simple-select");
-      return name.textContent;
+    const name = document.querySelector("#demo-simple-select");
+    return name.textContent;
   }
 
   const populateTasks = () => {
-    return (
-      tasks.map(task => (
-        <Task selectedUser={getSelectedUsername()} task={task}/>
-      ))
-    )
-  } 
+    return tasks.map((task) => (
+      <Task selectedUser={getSelectedUsername()} task={task} />
+    ));
+  };
 
   const populateUsers = () => {
-    return (
-      users.map(user => (
-        <MenuItem value={user.id}>{user.name}</MenuItem>
-      ))
-    )
-  }
+    return users.map((user) => (
+      <MenuItem value={user.id}>{user.name}</MenuItem>
+    ));
+  };
 
   return (
     <div className="App">
       <h1>Todo app</h1>
       <form>
         <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-label">Users</InputLabel>
+          <InputLabel id="demo-simple-select-label">Users</InputLabel>
           <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={inputUser}
-              onChange={event => setInputUser(event.target.value)}
-            >
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={inputUser}
+            onChange={(event) => setInputUser(event.target.value)}
+          >
             {populateUsers()}
           </Select>
         </FormControl>
         <FormControl>
           <InputLabel>Write a Todo</InputLabel>
-          <Input value={input} onChange={event => setInput(event.target.value)} />
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
         </FormControl>
-        <Button id="addTaskButton" disabled={!input.trim() || !inputUser} type="submit" onClick={addTask} variant="contained" color="primary">
+        <Button
+          id="addTaskButton"
+          disabled={!input.trim() || !inputUser}
+          type="submit"
+          onClick={addTask}
+          variant="contained"
+          color="primary"
+        >
           Add Task
         </Button>
       </form>
-        <Grid id="appGrid" container direction="column" alignItems="center" spacing={3}>
-            {populateTasks()}
-        </Grid>
-        <SnackbarComponent isActivated={handleSnackbar} handleClose={handleSnackbarClose} />
+      <Grid
+        id="appGrid"
+        container
+        direction="column"
+        alignItems="center"
+        spacing={3}
+      >
+        {populateTasks()}
+      </Grid>
+      <SnackbarComponent
+        isActivated={handleSnackbar}
+        handleClose={handleSnackbarClose}
+      />
     </div>
   );
 }
--- src/Task.js
+++ src/Task.js
@@ -1,174 +1,193 @@
-import React, { useEffect, useState }  from 'react'
-import { Button, ListItemText, Modal, Grid, Paper, Input, FormControlLabel, Checkbox } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import ColorPicker from 'material-ui-color-picker'
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  ListItemText,
+  Modal,
+  Grid,
+  Paper,
+  Input,
+  FormControlLabel,
+  Checkbox,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import ColorPicker from "material-ui-color-picker";
 
 //firebase and its config file
-import db from './firebase';
-import firebase from 'firebase'
+import db from "./firebase";
+import firebase from "firebase";
 
 //icons
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import EditIcon from '@material-ui/icons/Edit';
-
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import EditIcon from "@material-ui/icons/Edit";
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-      position: 'absolute',
-      width: '30%',
-      left: '50%',
-      transform: 'translate(-50%, 0)',
-      top: '25%',
-      backgroundColor: theme.palette.background.paper,
-      border: '1px solid black',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-    },
-    gridPaper: {
-        padding: theme.spacing(2),
-        textAlign: 'left'
-    },
-    icons: {
-        margin: '15px 0 0 35px',
-        cursor: 'pointer'
-    },
-    updateButton: {
-        marginLeft: "15px"
-    },
-    headerContainer: {
-        marginBottom: "15px"
-    },
-    header_2: {
-        display: "inline"
-    },
-    header_5 : {
-        display: "inline",
-        fontWeight: "normal",
-        marginLeft: "12px",
-        color: "gray",
-        fontStyle: "italic"
-    },
-    colorPicker: {
-        marginLeft: "15px"
-    }
-  }));
+  paper: {
+    position: "absolute",
+    width: "30%",
+    left: "50%",
+    transform: "translate(-50%, 0)",
+    top: "25%",
+    backgroundColor: theme.palette.background.paper,
+    border: "1px solid black",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  gridPaper: {
+    padding: theme.spacing(2),
+    textAlign: "left",
+  },
+  icons: {
+    margin: "15px 0 0 35px",
+    cursor: "pointer",
+  },
+  updateButton: {
+    marginLeft: "15px",
+  },
+  headerContainer: {
+    marginBottom: "15px",
+  },
+  header_2: {
+    display: "inline",
+  },
+  header_5: {
+    display: "inline",
+    fontWeight: "normal",
+    marginLeft: "12px",
+    color: "gray",
+    fontStyle: "italic",
+  },
+  colorPicker: {
+    marginLeft: "15px",
+  },
+}));
 
 export default function Task(props) {
-    const classes = useStyles();
-    const [open, setOpen] = useState(false);
-    const [input, setInput] = useState('');
-    const [inputColor, setInputColor] = useState('');
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState("");
+  const [inputColor, setInputColor] = useState("");
 
-    useEffect(() => {
-        try {
-            db.collection('tasks').doc(props.task.id).update({
-                taskColor: inputColor
-            })
-        } catch(error) {
-            alert(error);
-        }
-    
-      }, [inputColor]);
+  useEffect(() => {
+    try {
+      db.collection("tasks").doc(props.task.id).update({
+        taskColor: inputColor,
+      });
+    } catch (error) {
+      alert(error);
+    }
+  }, [inputColor]);
 
-    const updateTask = (event) => {
-        event.preventDefault();
-        
-        try {
-            db.collection('tasks').doc(props.task.id).update({
-                task: input,
-                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-                isCompleted: false
-            })
-        } catch(error) {
-            alert(error);
-        }
-        
-        setOpen(false);
+  const updateTask = (event) => {
+    event.preventDefault();
+
+    try {
+      db.collection("tasks").doc(props.task.id).update({
+        task: input,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        isCompleted: false,
+      });
+    } catch (error) {
+      alert(error);
     }
 
-    const convertDate = (timestamp) => {
-        if(timestamp){
-            const date = new Date(timestamp.seconds*1000).toLocaleString();
-            return date;
-        }
-    }
+    setOpen(false);
+  };
 
-    const deleteTask = () => {
-        try {
-            db.collection('tasks').doc(props.task.id).delete();
-        } catch(error) {
-            alert(error);
-        }
+  const convertDate = (timestamp) => {
+    if (timestamp) {
+      const date = new Date(timestamp.seconds * 1000).toLocaleString();
+      return date;
     }
+  };
 
-    const handleCheckboxChange = () => {
-        console.log(props.task)
-        try {
-            db.collection('tasks').doc(props.task.id).update({
-                isCompleted: !props.task.isCompleted
-            })
-        } catch(error) {
-            alert(error);
-        }
+  const deleteTask = () => {
+    try {
+      db.collection("tasks").doc(props.task.id).delete();
+    } catch (error) {
+      alert(error);
     }
+  };
 
-    const handleTaskColor = (color) => {
-        if(color){
-            setInputColor(color);
-        }
-            
+  const handleCheckboxChange = () => {
+    console.log(props.task);
+    try {
+      db.collection("tasks").doc(props.task.id).update({
+        isCompleted: !props.task.isCompleted,
+      });
+    } catch (error) {
+      alert(error);
     }
+  };
 
-    return (
-        <>
-        <Modal open={open} onClose={e => setOpen(false)}>
-            <div className={classes.paper}>
-                <div className={classes.headerContainer}>
-                    <h2 className={classes.header_2}>{props.task.task}</h2>
-                    <h5 className={classes.header_5}>by {props.selectedUser}</h5>
-                </div>
-                <form>
-                    <Input placeholder={props.task.task} onChange={event => setInput(event.target.value)} />
-                    <Button type="submit"
-                        className={classes.updateButton} 
-                        variant="contained" 
-                        color="primary" 
-                        disabled={!input.trim() || !input} 
-                        onClick={updateTask}>Update Task</Button>
-                </form>
-            </div>
-        </Modal>
-        <Grid item>
-          <Paper className={classes.gridPaper} style={{backgroundColor : props.task.taskColor}}>
-            <Grid container wrap="nowrap" spacing={2}>
-                <Grid item>
-                    <ListItemText primary={props.task.task} secondary={convertDate(props.task.timestamp)} />
-                    <FormControlLabel
-                    control={
-                    <Checkbox
-                        checked={props.task.isCompleted}
-                        onChange={handleCheckboxChange}
-                        name="checkedB"
-                        color="primary"
-                    />
-                    }
-                    label={props.task.isCompleted ? "Done!" : "Not Done"}
-                />
-                Task Color:
-                <ColorPicker
-                        className={classes.colorPicker}
-                        name='color'
-                        defaultValue={props.task.taskColor}
-                        onChange={handleTaskColor}
-                    />
-                </Grid>
-                <Grid item className={classes.icons}>
-                    <EditIcon onClick={e => setOpen(true)} />
-                    <DeleteForeverIcon onClick={deleteTask} />
-                </Grid>
+  const handleTaskColor = (color) => {
+    if (color) {
+      setInputColor(color);
+    }
+  };
+
+  return (
+    <>
+      <Modal open={open} onClose={(e) => setOpen(false)}>
+        <div className={classes.paper}>
+          <div className={classes.headerContainer}>
+            <h2 className={classes.header_2}>{props.task.task}</h2>
+            <h5 className={classes.header_5}>by {props.selectedUser}</h5>
+          </div>
+          <form>
+            <Input
+              placeholder={props.task.task}
+              onChange={(event) => setInput(event.target.value)}
+            />
+            <Button
+              type="submit"
+              className={classes.updateButton}
+              variant="contained"
+              color="primary"
+              disabled={!input.trim() || !input}
+              onClick={updateTask}
+            >
+              Update Task
+            </Button>
+          </form>
+        </div>
+      </Modal>
+      <Grid item>
+        <Paper
+          className={classes.gridPaper}
+          style={{ backgroundColor: props.task.taskColor }}
+        >
+          <Grid container wrap="nowrap" spacing={2}>
+            <Grid item>
+              <ListItemText
+                primary={props.task.task}
+                secondary={convertDate(props.task.timestamp)}
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={props.task.isCompleted}
+                    onChange={handleCheckboxChange}
+                    name="checkedB"
+                    color="primary"
+                  />
+                }
+                label={props.task.isCompleted ? "Done!" : "Not Done"}
+              />
+              Task Color:
+              <ColorPicker
+                className={classes.colorPicker}
+                name="color"
+                defaultValue={props.task.taskColor}
+                onChange={handleTaskColor}
+              />
             </Grid>
-          </Paper>
-        </Grid>
-        </>
-    )
+            <Grid item className={classes.icons}>
+              <EditIcon onClick={(e) => setOpen(true)} />
+              <DeleteForeverIcon onClick={deleteTask} />
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    </>
+  );
 }
